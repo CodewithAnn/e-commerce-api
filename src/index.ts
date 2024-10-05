aac257f1495f3193c21236159ab4912d29fa6071
CodeWithAnn <codewithann@gmail.com>
2024-10-05 23:11:25 +0530

    create auth route and test it

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from "express";
 import { PORT } from "./secrets.js";
+import rootRouter from "./routes/index.js";
 
 const app = express();
-// const port = 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is Running!!!!");
-});
+//routes
+app.use("/api", rootRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
